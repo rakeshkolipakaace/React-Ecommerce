91758ec41dc2f14b8fda883de3c2afcf0ec1f393
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-23 16:44:28 +0530

    src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,30 @@ import Product from './components/Product'
 
 import Navbar from './components/Navbar'
 
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import ProductDetail from './components/ProductDetail';
+
+import Cart from './components/Cart';
+
+import SearchItems from './components/SearchItems';
 const App = () => {
   return (
     <>
-    <Navbar />
+   <Router>
+     <Navbar />
+
+     <Routes>
+      <Route path="/" element={<Product />} />
+      <Route path="/product/:id" element={<ProductDetail />} />
+      <Route path="/search/:term" element={<SearchItems />} />
+      <Route path="/cart" element={<Cart />} />
+
+
+
+     </Routes>
     <Product />
+   </Router>
     </>
   )
 }
