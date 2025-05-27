681ae5fedd3c9e4d6828b43b3f3fb0efbe960467
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-27 18:42:39 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Product from './components/Product'
 
@@ -11,14 +11,18 @@ import ProductDetail from './components/ProductDetail';
 import Cart from './components/Cart';
 
 import SearchItems from './components/SearchItems';
+
+import {items} from './components/Data';
 const App = () => {
+
+  const[data,setData]=useState([...items])
   return (
     <>
    <Router>
      <Navbar />
 
      <Routes>
-      <Route path="/" element={<Product />} />
+      <Route path="/" element={<Product items={data} />} />
       <Route path="/product/:id" element={<ProductDetail />} />
       <Route path="/search/:term" element={<SearchItems />} />
       <Route path="/cart" element={<Cart />} />
