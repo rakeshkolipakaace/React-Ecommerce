4fb73de50559ecfca178122aec053e14eb88797c
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-27 18:43:30 +0530

    Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,31 +1,47 @@
 import React from 'react';
-import { items } from './Data';
 
 import {Link} from 'react-router-dom';
 
-const Product = () => {
+const Product = ({items}) => {
   return (
-    <div className="container">
-      <div className="row">
-        {
-          items.map((product, index) => (
-            <div key={product.id}className="col-lg-4 col-md-6 my-3 text-center">
-              <div className="card" style={{ width: '18rem' }}>
-                <Link to={`/product/${product.id}`}style={{display:'flex',justifyContent:'center',alignItems:'center'}}><img src={product.imgSrc} className="card-img-top" alt={product.title} /></Link>
-                <div className="card-body">
-                  <h5 className="card-title">{product.title}</h5>
-                  <p className="card-text">{product.description}</p>
-                  <button className="btn btn-primary mx-3">
-                    {product.price} ₹
-                  </button>
-                  <button className="btn btn-warning">Add To Cart</button>
+    <div className="container my-5">
+        <div className="row">
+          {items.map((product) => {
+            return (
+              <>
+                <div key={product.id} className="col-lg-4 col-md-6 my-3 text-center">
+                  <div className="card" style={{ width: "18rem" }}>
+                    <Link to={`/product/${product.id}`}
+                     style={{
+                        display:'flex',
+                        justifyContent:'center',
+                        alignItems:'center'
+                    }}>
+
+                    <img
+                      src={product.imgSrc}
+                      className="card-img-top"
+                      alt="..."
+                      />
+                      </Link>
+                    <div className="card-body">
+                      <h5 className="card-title">{product.title}</h5>
+                      <p className="card-text">{product.description}</p>
+                      <button className="btn btn-primary mx-3">
+                        {product.price} ₹
+                      </button>
+                      <button
+                      onClick={()=>addToCart(product.id,product.price,product.title,product.description,product.imgSrc)}
+                       className="btn btn-warning"
+                       >Add To Cart</button>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          ))
-        }
+              </>
+            );
+          })}
+        </div>
       </div>
-    </div>
   );
 };
 
